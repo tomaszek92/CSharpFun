8181deb145e4e47fd541ce2596284c120a9667d8
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2021-10-29 16:54:35 +0200

    StringValues

---
+++ CSharpFun.StringValues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpFun.StringValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empty = new Microsoft.Extensions.Primitives.StringValues();
+            var single = new Microsoft.Extensions.Primitives.StringValues("lorem");
+            var multiple = new Microsoft.Extensions.Primitives.StringValues(new[] { "ipsum", "dolor", "sit", "amet" });
+
+            Console.WriteLine(nameof(empty));
+            Display(empty);
+            
+            Console.WriteLine();
+            
+            Console.WriteLine(nameof(single));
+            Display(single);
+            
+            Console.WriteLine();
+            
+            Console.WriteLine(nameof(multiple));
+            Display(multiple);
+        }
+
+        private static void Display(Microsoft.Extensions.Primitives.StringValues strV)
+        {
+            Console.WriteLine(strV);
+            Console.WriteLine(strV.Count);
+            Console.WriteLine(strV.ToString());
+        }
+    }
+}
